61fe2e3136342539464eb278606c5a3f72b7847a
Nate Koch <nkoch@uoregon.edu>
2021-10-16 16:23:57 -0700

    implemented functions except move copy and delete need to make sure errors are handled and only sys calls used

--- Projects/project1/command.c
+++ Projects/project1/command.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
 #include "command.h"
 
 void listDir() {
-    getcwd(cwd, sizeof(cwd));
-    write(1, cwd, sizeof(cwd));
-    write(1, "\n", 1);
+    char *cwd = NULL;
+    char *currentDir = NULL;
+    currentDir = getcwd(cwd, 3000);
+
+    DIR *dr;
+    dr = opendir(currentDir);
+    struct dirent *de;
+
+    if (dr == NULL) {
+        printf("Could not open current directory");
+        exit(EXIT_FAILURE);
+    }
+
+    while((de = readdir(dr)) != NULL) {
+        write(1, de->d_name, strlen(de->d_name));
+        write(1, "\n", 1);
+    }
+
+    closedir(dr);
+    free(currentDir);
 } /*for the ls command*/
 
 
 void showCurrentDir() {
-
+    char *cwd = NULL;
+    char *currentDir = NULL;
+    currentDir = getcwd(cwd, 3000);
+    write(1, currentDir, strlen(currentDir));
+    write(1, "\n", 1);
+    free(currentDir);
 } /*for the pwd command*/
 
 void makeDir(char *dirName) {
-
+    mkdir(dirName, S_IRWXU);
 } /*for the mkdir command*/
 
 void changeDir(char *dirName) {
-
+    chdir(dirName);
 } /*for the cd command*/
 
 void copyFile(char *sourcePath, char *destinationPath) {
@@ -34,3 +61,15 @@ void deleteFile(char *filename) {
 
 } /*for the rm command*/
 
+void displayFile(char *filename) {
+    int fd;
+    fd = open(filename, O_RDONLY);
+    char *line_buf;
+    ssize_t  count;
+    line_buf = malloc(1024);
+    while ((count = read(fd, line_buf, 1024)) > 0) {
+        write(1, line_buf, count);
+    }
+    free(line_buf);
+    close(fd);
+} /*for the cat command*/
--- Projects/project1/main.c
+++ Projects/project1/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "command.h"
 #include "string_parser.h"
 
 int main(int argc, char *argv[]) {
+    bool filemode_on = false;
     listDir();
+    displayFile("string_parser.h");
     exit(EXIT_SUCCESS);
 }
 
