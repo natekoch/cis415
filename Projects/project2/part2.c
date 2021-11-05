d096264b17f9251745e771ffc133532d0c966594
Nate Koch <nkoch@uoregon.edu>
2021-11-04 20:26:43 -0700

    working part1 and part2 i think

--- Projects/project2/part2.c
+++ Projects/project2/part2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/wait.h>
 #include "string_parser.h"
 
@@ -32,6 +33,13 @@ int main(int argc, char *argv[]) {
     pid_t *pid_array;
     pid_array = malloc(sizeof(pid_t) * line_number);
 
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGUSR1);
+    sigprocmask(SIG_BLOCK, &sigset, NULL);
+
+    int sig;
+
     for (int i = 0; i < line_number; i++) {
         line_token_buffer = str_filler(lines[i], " ");
         pid_array[i] = fork();
@@ -39,12 +47,29 @@ int main(int argc, char *argv[]) {
             perror("fork");
         }
         if (pid_array[i] == 0) {
+            sigwait(&sigset, &sig);
             if (execvp(line_token_buffer.command_list[0], line_token_buffer.command_list) == -1) {
                 perror("execvp");
             }
+            printf("got signal: %ls\n", &sig);
             exit(-1);
         }
         free_command_line(&line_token_buffer);
+    }
+
+    // send sigusr1
+    for (int i = 0; i < line_number; i++) {
+        kill(pid_array[i], SIGUSR1);
+    }
+
+    // send sigstop
+    for (int i = 0; i < line_number; i++) {
+        kill(pid_array[i], SIGSTOP);
+    }
+
+    // send sigcont
+    for (int i = 0; i < line_number; i++) {
+        kill(pid_array[i], SIGCONT);
     }
 
     for (int i = 0; i < line_number; i++) {
